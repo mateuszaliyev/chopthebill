b380b00bb4ac3a64ee8fdb5816ca494072d351c0
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-04-26 02:27:20 +0200

    Dodanie zakończenia procesu po błędzie bazy danych

--- config/db.js
+++ config/db.js
@@ -2,4 +2,9 @@ const { Pool } = require("pg");
 
 const db = new Pool();
 
+db.on("error", (err, client) => {
+	console.error("Unexpected error on idle client", err);
+	process.exit(-1);
+});
+
 module.exports = { db };
