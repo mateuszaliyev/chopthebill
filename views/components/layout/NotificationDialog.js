5a12dbfa8e7008cb3d0ecbe1604ef33216578b5e
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-05-10 17:03:10 +0200

    Dodanie responsywności okna powiadomień

--- views/components/layout/NotificationDialog.js
+++ views/components/layout/NotificationDialog.js
@@ -1,9 +1,43 @@
-import { Dialog, DialogContent, DialogTitle } from "@material-ui/core";
+import {
+	Dialog,
+	DialogContent,
+	DialogTitle,
+	IconButton,
+	useMediaQuery,
+} from "@material-ui/core";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import CloseIcon from "@material-ui/icons/Close";
+
+const useStyles = makeStyles((theme) => ({
+	closeButton: {
+		position: "absolute",
+		right: theme.spacing(1),
+		top: theme.spacing(1),
+	},
+}));
 
 function NotificationDialog({ onClose, open, title }) {
+	const classes = useStyles();
+	const theme = useTheme();
+	const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
 	return (
-		<Dialog onClose={onClose} open={open}>
-			<DialogTitle>{title}</DialogTitle>
+		<Dialog
+			fullScreen={fullScreen}
+			fullWidth={true}
+			onClose={onClose}
+			open={open}
+		>
+			<DialogTitle>
+				{title}
+				<IconButton
+					aria-label="close"
+					className={classes.closeButton}
+					onClick={onClose}
+				>
+					<CloseIcon />
+				</IconButton>
+			</DialogTitle>
 			<DialogContent dividers>TODO: {title}</DialogContent>
 		</Dialog>
 	);
