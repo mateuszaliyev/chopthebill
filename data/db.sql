c6b5d5b97063cd80f5ab995113ed76f25fdb24df
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-05-11 15:20:52 +0200

    Aktualizacja skryptu tworzącego bazę danych

--- data/db.sql
+++ data/db.sql
@@ -1,7 +1,3 @@
--- Database: chopthebill
-
--- DROP DATABASE chopthebill;
-
 CREATE DATABASE chopthebill
 	WITH 
 	OWNER = postgres
@@ -11,152 +7,83 @@ CREATE DATABASE chopthebill
 	TABLESPACE = pg_default
 	CONNECTION LIMIT = -1;
 
-DROP TABLE IF EXISTS public.affiliation;
-DROP TABLE IF EXISTS public.expense;
-DROP TABLE IF EXISTS public.friendship;
-DROP TABLE IF EXISTS public.group;
-DROP TABLE IF EXISTS public.notification;
-DROP TABLE IF EXISTS public.obligation;
-DROP TABLE IF EXISTS public.user;
 
--- Table: public.affiliation
-
--- DROP TABLE public.affiliation;
 
 CREATE TABLE public.affiliation
 (
-	id_affiliation bigint NOT NULL DEFAULT nextval('affiliation_id_affiliation_seq'::regclass),
+	id_affiliation bigserial NOT NULL,
 	owner boolean NOT NULL,
 	valid boolean NOT NULL,
 	id_user bigint NOT NULL,
 	id_group bigint NOT NULL,
-	CONSTRAINT affiliation_pkey PRIMARY KEY (id_affiliation)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.affiliation
-	OWNER to postgres;
-
--- Table: public.expense
-
--- DROP TABLE public.expense;
+	PRIMARY KEY (id_affiliation)
+);
 
 CREATE TABLE public.expense
 (
-	id_expense bigint NOT NULL DEFAULT nextval('expense_id_expense_seq'::regclass),
-	name character varying(63) COLLATE pg_catalog."default" NOT NULL,
-	description text COLLATE pg_catalog."default",
+	id_expense bigserial NOT NULL,
+	name character varying(63) NOT NULL,
+	description text,
 	date timestamp with time zone NOT NULL,
 	value bigint NOT NULL,
-	currency character varying(3) COLLATE pg_catalog."default" NOT NULL,
+	currency character varying(3) NOT NULL,
 	settled boolean NOT NULL,
 	id_user bigint NOT NULL,
 	id_group bigint,
-	CONSTRAINT expense_pkey PRIMARY KEY (id_expense)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.expense
-	OWNER to postgres;
-
--- Table: public.friendship
-
--- DROP TABLE public.friendship;
+	PRIMARY KEY (id_expense)
+);
 
 CREATE TABLE public.friendship
 (
-	id_friendship bigint NOT NULL DEFAULT nextval('friendship_id_friendship_seq'::regclass),
+	id_friendship bigserial NOT NULL,
 	valid boolean NOT NULL,
 	id_user_1 bigint NOT NULL,
 	id_user_2 bigint NOT NULL,
-	CONSTRAINT friendship_pkey PRIMARY KEY (id_friendship)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.friendship
-	OWNER to postgres;
-
--- Table: public.group
-
--- DROP TABLE public."group";
+	PRIMARY KEY (id_friendship)
+);
 
 CREATE TABLE public."group"
 (
-	id_group bigint NOT NULL DEFAULT nextval('group_id_group_seq'::regclass),
-	name character varying(63) COLLATE pg_catalog."default" NOT NULL,
-	description text COLLATE pg_catalog."default",
+	id_group bigserial NOT NULL,
+	name character varying(63) NOT NULL,
+	description text,
 	deleted boolean NOT NULL,
-	CONSTRAINT group_pkey PRIMARY KEY (id_group)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public."group"
-	OWNER to postgres;
-
--- Table: public.notification
-
--- DROP TABLE public.notification;
+	PRIMARY KEY (id_group)
+);
 
 CREATE TABLE public.notification
 (
-	id_notification bigint NOT NULL DEFAULT nextval('notification_id_notification_seq'::regclass),
-	title character varying(255) COLLATE pg_catalog."default" NOT NULL,
-	description text COLLATE pg_catalog."default",
-	redirect text COLLATE pg_catalog."default" NOT NULL,
+	id_notification bigserial NOT NULL,
+	title character varying(255) NOT NULL,
+	description text,
+	redirect text NOT NULL,
 	read boolean NOT NULL,
 	id_user bigint NOT NULL,
-	CONSTRAINT notification_pkey PRIMARY KEY (id_notification)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.notification
-	OWNER to postgres;
-
--- Table: public.obligation
-
--- DROP TABLE public.obligation;
+	PRIMARY KEY (id_notification)
+);
 
 CREATE TABLE public.obligation
 (
-    id_obligation bigint NOT NULL DEFAULT nextval('obligation_id_obligation_seq'::regclass),
+    id_obligation bigserial NOT NULL,
     value bigint NOT NULL,
     settled boolean NOT NULL,
     id_user_debtor bigint NOT NULL,
     id_user_creditor bigint NOT NULL,
     id_expense bigint NOT NULL,
-    CONSTRAINT obligation_pkey PRIMARY KEY (id_obligation)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public.obligation
-    OWNER to postgres;
-
--- Table: public.user
-
--- DROP TABLE public."user";
+    PRIMARY KEY (id_obligation)
+);
 
 CREATE TABLE public."user"
 (
-	id_user bigint NOT NULL DEFAULT nextval('user_id_user_seq'::regclass),
-	email character varying(63) COLLATE pg_catalog."default" NOT NULL,
-	password character varying(255) COLLATE pg_catalog."default" NOT NULL,
-	username character varying(63) COLLATE pg_catalog."default" NOT NULL,
-	language character varying(15) COLLATE pg_catalog."default" NOT NULL,
-	theme character varying(63) COLLATE pg_catalog."default" NOT NULL,
+	id_user bigserial NOT NULL,
+	email character varying(63) NOT NULL,
+	password character varying(255) NOT NULL,
+	username character varying(63) NOT NULL,
+	language character varying(15) NOT NULL,
+	theme character varying(63) NOT NULL,
 	hide_email boolean NOT NULL,
 	last_seen timestamp with time zone NOT NULL,
-	refresh_token character varying(255) COLLATE pg_catalog."default",
+	refresh_token character varying(255),
 	deleted boolean NOT NULL,
-	CONSTRAINT user_pkey PRIMARY KEY (id_user)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE public."user"
-	OWNER to postgres;
+	PRIMARY KEY (id_user)
+);
