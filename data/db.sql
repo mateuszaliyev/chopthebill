ccee2e5897f40fcd735b5a94fb388d9230de7d4e
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-05-18 01:10:41 +0200

    Dodanie kluczy obcych do skryptu tworzącego bazę

--- data/db.sql
+++ data/db.sql
@@ -9,71 +9,7 @@ CREATE DATABASE chopthebill
 
 
 
-CREATE TABLE public.affiliation
-(
-	id_affiliation bigserial NOT NULL,
-	owner boolean NOT NULL,
-	valid boolean NOT NULL,
-	id_user bigint NOT NULL,
-	id_group bigint NOT NULL,
-	PRIMARY KEY (id_affiliation)
-);
-
-CREATE TABLE public.expense
-(
-	id_expense bigserial NOT NULL,
-	name character varying(63) NOT NULL,
-	description text,
-	date timestamp with time zone NOT NULL,
-	value bigint NOT NULL,
-	currency character varying(3) NOT NULL,
-	settled boolean NOT NULL,
-	id_user bigint NOT NULL,
-	id_group bigint,
-	PRIMARY KEY (id_expense)
-);
-
-CREATE TABLE public.friendship
-(
-	id_friendship bigserial NOT NULL,
-	valid boolean NOT NULL,
-	id_user_1 bigint NOT NULL,
-	id_user_2 bigint NOT NULL,
-	PRIMARY KEY (id_friendship)
-);
-
-CREATE TABLE public."group"
-(
-	id_group bigserial NOT NULL,
-	name character varying(63) NOT NULL,
-	description text,
-	deleted boolean NOT NULL,
-	PRIMARY KEY (id_group)
-);
-
-CREATE TABLE public.notification
-(
-	id_notification bigserial NOT NULL,
-	title character varying(255) NOT NULL,
-	description text,
-	redirect text NOT NULL,
-	read boolean NOT NULL,
-	id_user bigint NOT NULL,
-	PRIMARY KEY (id_notification)
-);
-
-CREATE TABLE public.obligation
-(
-    id_obligation bigserial NOT NULL,
-    value bigint NOT NULL,
-    settled boolean NOT NULL,
-    id_user_debtor bigint NOT NULL,
-    id_user_creditor bigint NOT NULL,
-    id_expense bigint NOT NULL,
-    PRIMARY KEY (id_obligation)
-);
-
-CREATE TABLE public."user"
+CREATE TABLE public.user
 (
 	id_user bigserial NOT NULL,
 	email character varying(63) NOT NULL,
@@ -86,4 +22,68 @@ CREATE TABLE public."user"
 	refresh_token character varying(255),
 	deleted boolean NOT NULL,
 	PRIMARY KEY (id_user)
+);
+
+CREATE TABLE public.group
+(
+	id_group bigserial NOT NULL,
+	name character varying(63) NOT NULL,
+	description text,
+	deleted boolean NOT NULL,
+	PRIMARY KEY (id_group)
+);
+
+CREATE TABLE public.affiliation
+(
+	id_affiliation bigserial NOT NULL,
+	owner boolean NOT NULL,
+	valid boolean NOT NULL,
+	id_user bigint NOT NULL REFERENCES public.user (id_user),
+	id_group bigint NOT NULL REFERENCES public.group (id_group),
+	PRIMARY KEY (id_affiliation)
+);
+
+CREATE TABLE public.expense
+(
+	id_expense bigserial NOT NULL,
+	name character varying(63) NOT NULL,
+	description text,
+	date timestamp with time zone NOT NULL,
+	value bigint NOT NULL,
+	currency character varying(3) NOT NULL,
+	settled boolean NOT NULL,
+	id_user bigint NOT NULL REFERENCES public.user (id_user),
+	id_group bigint REFERENCES public.group (id_group),
+	PRIMARY KEY (id_expense)
+);
+
+CREATE TABLE public.friendship
+(
+	id_friendship bigserial NOT NULL,
+	valid boolean NOT NULL,
+	id_user_1 bigint NOT NULL REFERENCES public.user (id_user),
+	id_user_2 bigint NOT NULL REFERENCES public.user (id_user),
+	PRIMARY KEY (id_friendship)
+);
+
+CREATE TABLE public.notification
+(
+	id_notification bigserial NOT NULL,
+	title character varying(255) NOT NULL,
+	description text,
+	redirect text NOT NULL,
+	read boolean NOT NULL,
+	id_user bigint NOT NULL REFERENCES public.user (id_user),
+	PRIMARY KEY (id_notification)
+);
+
+CREATE TABLE public.obligation
+(
+    id_obligation bigserial NOT NULL,
+    value bigint NOT NULL,
+    settled boolean NOT NULL,
+    id_user_debtor bigint NOT NULL REFERENCES public.user (id_user),
+    id_user_creditor bigint NOT NULL REFERENCES public.user (id_user),
+    id_expense bigint NOT NULL REFERENCES public.expense (id_expense),
+    PRIMARY KEY (id_obligation)
 );
