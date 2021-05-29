8849f6db9ad50720d315bbe45c3c71082c32b484
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-05-29 17:30:57 +0200

    Przygotowanie aplikacji do deployu

--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const { Pool } = require("pg");
 
-const db = new Pool();
+const connectionString =
+	process.env.DATABASE_URL && `${process.env.DATABASE_URL}?sslmode=require`;
+const dev = process.env.NODE_ENV !== "production";
+
+const db =
+	connectionString && !dev ? new Pool({ connectionString }) : new Pool();
 
 db.on("error", (err, client) => {
 	console.error("Unexpected error on idle client", err);
--- views/config/index.js
+++ views/config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const host = dev
 	? `http://localhost:5000` //  development
-	: `http://localhost:5000`; // production
+	: `https://chopthebill.herokuapp.com`; // production
