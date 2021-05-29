b7eb8e1e6e91bb12f005745d957777a4373d4fec
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-05-29 19:21:15 +0200

    Konfiguracja połączenia z bazą danych na Heroku

--- config/db.js
+++ config/db.js
@@ -1,12 +1,17 @@
-require("dotenv").config();
 const { Pool } = require("pg");
 
-const connectionString =
-	process.env.DATABASE_URL && `${process.env.DATABASE_URL}?sslmode=require`;
+const connectionString = process.env.DATABASE_URL;
 const dev = process.env.NODE_ENV !== "production";
 
 const db =
-	connectionString && !dev ? new Pool({ connectionString }) : new Pool();
+	connectionString && !dev
+		? new Pool({
+				connectionString,
+				ssl: {
+					rejectUnauthorized: false,
+				},
+		  })
+		: new Pool();
 
 db.on("error", (err, client) => {
 	console.error("Unexpected error on idle client", err);
