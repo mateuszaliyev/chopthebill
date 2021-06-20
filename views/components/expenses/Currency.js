fe9f2c03d62b57b61fe602dfbf98e6c2862188ec
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2021-06-20 05:14:31 +0200

    Dodanie twardej spacji między walutą a kwotą

--- views/components/expenses/Currency.js
+++ views/components/expenses/Currency.js
@@ -1,6 +1,17 @@
 // Currencies
 import currencies from "../../config/currencies";
 
+export const getCurrencyString = (amount, code) => {
+	const currency = currencies.find((currency) => currency.code === code);
+	return `${amount < 0 ? "-" : ""}${
+		currency.position === "before" ? currency.symbol : ""
+	}${currency.position === "before" && currency.space ? " " : ""}${parseFloat(
+		Math.abs(amount) || 0
+	).toFixed(2)}${currency.position === "after" && currency.space ? " " : ""}${
+		currency.position === "after" ? currency.symbol : ""
+	}`;
+};
+
 function Currency({ amount, code }) {
 	const currency = currencies.find((currency) => currency.code === code);
 
@@ -8,9 +19,9 @@ function Currency({ amount, code }) {
 		<span>
 			{amount < 0 && "-"}
 			{currency.position === "before" && currency.symbol}
-			{currency.position === "before" && currency.space && " "}
+			{currency.position === "before" && currency.space && "\u00A0"}
 			{parseFloat(Math.abs(amount) || 0).toFixed(2)}
-			{currency.position === "after" && currency.space && " "}
+			{currency.position === "after" && currency.space && "\u00A0"}
 			{currency.position === "after" && currency.symbol}
 		</span>
 	);
